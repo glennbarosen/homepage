a405c1b3612b4b45bcfb8fd1a64dd817f9e23d39
Glenn Arnold Barosen <glennbarosen@gmail.com>
2024-04-25 12:42:40 +0200

    Add contact page with contact information

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Kontakt - Glenn.</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<main>
+	<a href="mailto:glennbarosen@gmail.com">glennbarosen@gmail.com</a>
+	<a href="tel:92822779">+47 928 22 779</a>
+	<a href="https://www.linkedin.com/in/glenn-barosen" target="_blank" rel="noopener noreferrer">
+		LinkedIn
+	</a>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+	a {
+		font-size: var(--font-large);
+		font-weight: 100;
+	}
+</style>
